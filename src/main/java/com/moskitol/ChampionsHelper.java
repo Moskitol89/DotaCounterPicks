34d08e2be24a2a12311b7a6c4047382f2320cf24
Moskitol89 <kurganovv@yahoo.com>
2018-01-23 19:01:50 +0300

    few changes + Jsoup

--- src/main/java/com/moskitol/ChampionsHelper.java
+++ src/main/java/com/moskitol/ChampionsHelper.java
@@ -3,25 +3,53 @@ package com.moskitol;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChampionsHelper {
 
-    private String fileName;
     private ClassLoader classLoader = this.getClass().getClassLoader();
+    private String en = fromFileToString("champions_en");
+    private String ru = fromFileToString("champions_ru");
 
-    public ChampionsHelper(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String fromFileToString() {
+    private String fromFileToString(String fileName) {
         String s = "";
         try{
             //обрезаем первые символы, c ними строка выглядит так:"file:/D:/projects/...."
             s = new String(Files.readAllBytes(Paths.get(String.valueOf(classLoader.getResource(fileName)).substring(6))));
-
         } catch (IOException e) {
             e.printStackTrace();
         }
         return s;
+    }
+
+    public ArrayList<Champion> initChampCollection() {
+        System.out.println(en);
+
+        System.out.println();
+
+        System.out.println(ru);
+
+        //сплитим строку полученную из файла ресурсов по - или переносу строки.
+        String[] enArray = en.split("-|\r\n");
+        String[] ruArray = ru.split("-|\r\n");
+
+        System.out.println(Arrays.toString(enArray));
+
+        ArrayList<Champion> championArrayList = new ArrayList<>();
+
+        //создаем и добавляем в коллекцию всех героев с именами из файла ресурсов.
+        for(String s : enArray) {
+            championArrayList.add(new Champion(s));
+        }
+        //добавляем героям альтернативные названия на русском языке.
+        for(int i = 0; i < enArray.length; i++) {
+            championArrayList.get(i).setRuName(ruArray[i]);
+        }
+
+        for(Champion champion: championArrayList) {
+            System.out.println(champion.getName() + "-" + champion.getRuName());
+        }
+        return championArrayList;
     }
 }
--- src/main/java/com/moskitol/Main.java
+++ src/main/java/com/moskitol/Main.java
@@ -1,41 +1,26 @@
 package com.moskitol;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        ChampionsHelper championsHelperEn = new ChampionsHelper("champions_en");
-        String en = championsHelperEn.fromFileToString();
-        System.out.println(en);
-
-        System.out.println();
-
-        ChampionsHelper championsHelperRu = new ChampionsHelper("champions_ru");
-        String ru = championsHelperRu.fromFileToString();
-        System.out.println(ru);
-
-        //сплитим строку полученную из файла ресурсов по - или переносу строки.
-        String[] enArray = en.split("-|\r\n");
-        String[] ruArray = ru.split("-|\r\n");
-
-        System.out.println(Arrays.toString(enArray));
-
-        ArrayList<Champion> championArrayList = new ArrayList<>();
-
-        //создаем и добавляем в коллекцию всех героев с именами из файла ресурсов.
-        for(String s : enArray) {
-            championArrayList.add(new Champion(s));
-        }
-        //добавляем героям альтернативные названия на русском языке.
-        for(int i = 0; i < enArray.length; i++) {
-            championArrayList.get(i).setRuName(ruArray[i]);
-        }
-
-        for(Champion champion: championArrayList) {
-            System.out.println(champion.getName() + "-" + champion.getRuName());
+        ArrayList<Champion> championArrayList = new ChampionsHelper().initChampCollection();
+        Document document;
+        try {
+            //получаем dom страницы героя с dotabuff.
+            document = Jsoup.connect("https://ru.dotabuff.com/heroes/shadow-fiend").get();
+            //получаем секцию с "силен против на этой неделе"
+            Elements elementGoodVs = document.select("div.col-8").select("section:eq(6)");
+            System.out.println(elementGoodVs);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
 }
